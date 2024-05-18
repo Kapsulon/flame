9e119bdc8aeea917fdb097d34b3b7acf2aba3440
Nathanaël Kimbembe <114685166+Nathanael-Kimbembe@users.noreply.github.com>
2024-05-18 18:42:52 +0200

    [ADD] random drop to monster

--- scripts/chamallow.gd
+++ scripts/chamallow.gd
@@ -5,6 +5,13 @@ var anim : AnimatedSprite2D
 @export var maxLife : float
 var alive : bool = true
 
+static var monster := preload("res://scenes/consumable/heal.tscn")
+static var deo := preload("res://scenes/consumable/deo.tscn")
+static var fireRate := preload("res://scenes/consumable/FireRateUp.tscn")
+static var fireRange := preload("res://scenes/consumable/FireRangeUp.tscn")
+static var strenght := preload("res://scenes/consumable/strenghtUp.tscn")
+static var epiShield := preload("res://scenes/consumable/epiShield.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     anim = get_node("AnimatedSprite2D")
@@ -28,6 +35,26 @@ func changeAnim(angle : float):
         scale.x = -1
         anim.animation = "side"
 
+func drop():
+    print("hello")
+    randomize()
+    var value : int = randi() % 11
+    var obj
+    if (value <= 2):
+        obj = monster.instantiate()
+    elif (value <= 4):
+        obj = deo.instantiate()
+    elif (value <= 6):
+        obj = strenght.instantiate()
+    elif (value <= 8):
+        obj = epiShield.instantiate()
+    elif (value <= 9):
+        obj = fireRate.instantiate()
+    else:
+        obj = fireRange.instantiate()
+    obj.position = position
+    get_tree().root.add_child(obj)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
     if (alive):
@@ -35,4 +62,7 @@ func _physics_process(delta):
     else:
         anim.self_modulate.a -= delta
         if (anim.self_modulate.a <= 0):
+            randomize()
+            if (randi() % 101 <= 10):
+                drop()
             queue_free()
