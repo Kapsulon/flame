77e5964fd5377365d0912ecb120df10aa6c3e411
Nathanaël Kimbembe <114685166+Nathanael-Kimbembe@users.noreply.github.com>
2024-05-18 13:58:45 +0200

    [FIX] flame script location

---
+++ scripts/flame.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var _dirVect : Vector2 = Vector2(0, 0)
+@export var _speed : float = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	setDirVect(_dirVect)
+
+func setDirVect(vect : Vector2):
+	_dirVect = vect
+	_dirVect.normalized()
+	var angle = atan(_dirVect.x / _dirVect.y)
+	rotate(angle)
+	if (_dirVect.x < 0):
+		scale.x = -scale.x
+
+func setSpeed(speed : float):
+	_speed = speed 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	position += _dirVect * _speed * delta
+
+
+func _on_animated_sprite_2d_animation_finished():
+	queue_free()
