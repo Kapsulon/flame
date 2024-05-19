d99823e34ea0b5f5a5421c181c1533940f679f4f
Your Name <you@example.com>
2024-05-19 15:21:47 +0200

    [ADD] Pathfinding

--- scripts/chamallow.gd
+++ scripts/chamallow.gd
@@ -5,6 +5,10 @@ var anim : AnimatedSprite2D
 @export var maxLife : float
 var alive : bool = true
 
+const speed = 150
+@export var player: Player
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
 static var monster := preload("res://scenes/consumable/heal.tscn")
 static var deo := preload("res://scenes/consumable/deo.tscn")
 static var fireRate := preload("res://scenes/consumable/FireRateUp.tscn")
@@ -14,55 +18,65 @@ static var epiShield := preload("res://scenes/consumable/epiShield.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    anim = get_node("AnimatedSprite2D")
+	anim = get_node("AnimatedSprite2D")
 
 func hit(damage : float):
-    life -= damage
-    if (life <= 0):
-        alive = false
-    var percent : float = life / maxLife
-    anim.self_modulate = Color.SADDLE_BROWN * (1 - percent) + Color.WHITE * percent
+	life -= damage
+	if (life <= 0):
+		alive = false
+	var percent : float = life / maxLife
+	anim.self_modulate = Color.SADDLE_BROWN * (1 - percent) + Color.WHITE * percent
 
 func changeAnim(angle : float):
-    if (angle < PI / 2 and angle > -PI / 2):
-        scale.x = 1
-        anim.animation = "side"
-    elif (angle < 3 * (PI / 2) && angle >= PI / 2):
-        anim.animation = "up"
-    elif (angle < -3 * (PI / 2) && angle >= -PI / 2):
-        anim.animation = "down"
-    else:
-        scale.x = -1
-        anim.animation = "side"
+	print(angle)
+	if (angle < PI / 4 and angle > -PI / 4):
+		$AnimatedSprite2D.flip_h = false
+		anim.animation = "side"
+	elif (angle < 3 * (PI / 4) && angle >= PI / 4):
+		anim.animation = "down"
+	elif (angle > -3 * (PI / 4) && angle <= -PI / 4):
+		anim.animation = "up"
+	else:
+		$AnimatedSprite2D.flip_h = true
+		anim.animation = "side"
 
 func drop():
-    print("hello")
-    randomize()
-    var value : int = randi() % 11
-    var obj
-    if (value <= 2):
-        obj = monster.instantiate()
-    elif (value <= 4):
-        obj = deo.instantiate()
-    elif (value <= 6):
-        obj = strenght.instantiate()
-    elif (value <= 8):
-        obj = epiShield.instantiate()
-    elif (value <= 9):
-        obj = fireRate.instantiate()
-    else:
-        obj = fireRange.instantiate()
-    obj.position = position
-    get_tree().root.add_child(obj)
+	print("hello")
+	randomize()
+	var value : int = randi() % 11
+	var obj
+	if (value <= 2):
+		obj = monster.instantiate()
+	elif (value <= 4):
+		obj = deo.instantiate()
+	elif (value <= 6):
+		obj = strenght.instantiate()
+	elif (value <= 8):
+		obj = epiShield.instantiate()
+	elif (value <= 9):
+		obj = fireRate.instantiate()
+	else:
+		obj = fireRange.instantiate()
+	obj.position = position
+	get_tree().root.add_child(obj)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-    if (alive):
-        changeAnim(velocity.angle())
-    else:
-        anim.self_modulate.a -= delta
-        if (anim.self_modulate.a <= 0):
-            randomize()
-            if (randi() % 101 <= 10):
-                drop()
-            queue_free()
+func _physics_process(delta : float) -> void:
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * speed
+	move_and_slide()
+	if (alive):
+		changeAnim(velocity.angle())
+	else:
+		anim.self_modulate.a -= delta
+		if (anim.self_modulate.a <= 0):
+			randomize()
+			if (randi() % 101 <= 10):
+				drop()
+			queue_free()
+
+func makepath() -> void:
+	nav_agent.target_position = player.global_position
+
+func _on_timer_timeout():
+	makepath()
