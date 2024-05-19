87dda6402ee3bca13431fe156916a991a3ce64c4
Nathanaël Kimbembe <114685166+Nathanael-Kimbembe@users.noreply.github.com>
2024-05-19 12:02:02 +0200

    [ADD] collision management in flame

--- scripts/FireRangeUp.gd
+++ scripts/FireRangeUp.gd
@@ -4,5 +4,4 @@ extends Node2D
 
 func _on_area_2d_body_entered(body):
     if (body.is_in_group("player")):
-        var player : Player = body.get_parent()
-        player.fireRange += fireRangeAdd
+        body.fireRange += fireRangeAdd
