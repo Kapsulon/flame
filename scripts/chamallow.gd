23f70b686498f6a8d5daddb09127a3e437733562
unknown <martinvergues@gmail.com>
2024-05-19 11:52:56 +0200

    Add do_bar decreasing

--- scripts/chamallow.gd
+++ scripts/chamallow.gd
@@ -14,55 +14,55 @@ static var epiShield := preload("res://scenes/consumable/epiShield.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    anim = get_node("AnimatedSprite2D")
+	anim = get_node("AnimatedSprite2D")
 
 func hit(damage : float):
-    life -= damage
-    if (life <= 0):
-        alive = false
-    var percent : float = life / maxLife
-    anim.self_modulate = Color.SADDLE_BROWN * (1 - percent) + Color.WHITE * percent
+	life -= damage
+	if (life <= 0):
+		alive = false
+	var percent : float = life / maxLife
+	anim.self_modulate = Color.SADDLE_BROWN * (1 - percent) + Color.WHITE * percent
 
 func changeAnim(angle : float):
-    if (angle < PI / 2 and angle > -PI / 2):
-        scale.x = 1
-        anim.animation = "side"
-    elif (angle < 3 * (PI / 2) && angle >= PI / 2):
-        anim.animation = "up"
-    elif (angle < -3 * (PI / 2) && angle >= -PI / 2):
-        anim.animation = "down"
-    else:
-        scale.x = -1
-        anim.animation = "side"
+	if (angle < PI / 2 and angle > -PI / 2):
+		scale.x = 1
+		anim.animation = "side"
+	elif (angle < 3 * (PI / 2) && angle >= PI / 2):
+		anim.animation = "up"
+	elif (angle < -3 * (PI / 2) && angle >= -PI / 2):
+		anim.animation = "down"
+	else:
+		scale.x = -1
+		anim.animation = "side"
 
 func drop():
-    print("hello")
-    randomize()
-    var value : int = randi() % 11
-    var obj
-    if (value <= 2):
-        obj = monster.instantiate()
-    elif (value <= 4):
-        obj = deo.instantiate()
-    elif (value <= 6):
-        obj = strenght.instantiate()
-    elif (value <= 8):
-        obj = epiShield.instantiate()
-    elif (value <= 9):
-        obj = fireRate.instantiate()
-    else:
-        obj = fireRange.instantiate()
-    obj.position = position
-    get_tree().root.add_child(obj)
+	print("hello")
+	randomize()
+	var value : int = randi() % 11
+	var obj
+	if (value <= 2):
+		obj = monster.instantiate()
+	elif (value <= 4):
+		obj = deo.instantiate()
+	elif (value <= 6):
+		obj = strenght.instantiate()
+	elif (value <= 8):
+		obj = epiShield.instantiate()
+	elif (value <= 9):
+		obj = fireRate.instantiate()
+	else:
+		obj = fireRange.instantiate()
+	obj.position = position
+	get_tree().root.add_child(obj)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-    if (alive):
-        changeAnim(velocity.angle())
-    else:
-        anim.self_modulate.a -= delta
-        if (anim.self_modulate.a <= 0):
-            randomize()
-            if (randi() % 101 <= 10):
-                drop()
-            queue_free()
+	if (alive):
+		changeAnim(velocity.angle())
+	else:
+		anim.self_modulate.a -= delta
+		if (anim.self_modulate.a <= 0):
+			randomize()
+			if (randi() % 101 <= 10):
+				drop()
+			queue_free()
--- scripts/torchswing.gd
+++ scripts/torchswing.gd
@@ -4,16 +4,16 @@ extends Node2D
 var swinging: bool = false
 
 func _ready() -> void:
-    $pivot/Torch.self_modulate.a = 0
+	$pivot/Torch.self_modulate.a = 0
 
 func swing() -> bool:
-    if swinging: return false
-    swinging = true
-    $pivot/Torch.self_modulate.a = 255
-    $pivot/Torch/burning_slash.restart()
-    $pivot/Torch/flame_particle.restart()
-    $AnimationPlayer.play("swing")
-    await $AnimationPlayer.animation_finished
-    $pivot/Torch.self_modulate.a = 0
-    swinging = false
-    return true
+	if swinging: return false
+	swinging = true
+	$pivot/Torch.self_modulate.a = 255
+	$pivot/Torch/burning_slash.restart()
+	$pivot/Torch/flame_particle.restart()
+	$AnimationPlayer.play("swing")
+	await $AnimationPlayer.animation_finished
+	$pivot/Torch.self_modulate.a = 0
+	swinging = false
+	return true
